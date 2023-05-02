51fd6b8a10836ed3eec8b2a1ac8ea741eb019145
LeoIsidro <90939274+LeoIsidro@users.noreply.github.com>
2023-05-02 13:22:06 -0500

    update app

--- fronted/src/App.vue
+++ fronted/src/App.vue
@@ -9,7 +9,7 @@ import HelloWorld from './components/HelloWorld.vue'
     <img alt="Vue logo" class="logo" src="@/assets/UTEC_LOGO.png" width="125" height="125" />
 
     <div >
-      <HelloWorld msg="Higher Lower View" />
+      <HelloWorld msg="Adivina la Chiquimusic" />
 
       <nav>
         <RouterLink to="/lobby">Lobby</RouterLink>
